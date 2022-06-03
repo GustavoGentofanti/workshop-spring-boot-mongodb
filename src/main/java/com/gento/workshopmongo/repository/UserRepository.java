5165026a2a8265689b9cfdc4c46516267293ea42
GustavoGentofanti <gustavogentofanti.pro@gmail.com>
2022-06-03 18:16:55 -0300

    Connecting to MongoDB with Repository and Service

---
+++ src/main/java/com/gento/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.gento.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gento.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
