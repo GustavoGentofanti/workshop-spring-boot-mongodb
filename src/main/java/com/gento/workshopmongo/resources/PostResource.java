0eb48e010bbad847c493f40d00edb8b2a6c99b83
GustavoGentofanti <gustavogentofanti.pro@gmail.com>
2022-06-07 19:15:15 -0300

    Documenting with Swagger

--- src/main/java/com/gento/workshopmongo/resources/PostResource.java
+++ src/main/java/com/gento/workshopmongo/resources/PostResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,20 +16,27 @@ import com.gento.workshopmongo.domain.Post;
 import com.gento.workshopmongo.resources.util.URL;
 import com.gento.workshopmongo.services.PostService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value="/posts")
+@Api(value="API REST Posts")
+@CrossOrigin(origins="*")
 public class PostResource {
 	
 	@Autowired
 	private PostService postService;
 	
 	@GetMapping(value="/{id}")
+	@ApiOperation(value="Retorna um único post")
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post post = postService.findById(id);
 		return ResponseEntity.ok().body(post);
 	}
 	
 	@GetMapping(value="/titlesearch")
+	@ApiOperation(value="Retorna um post pelo título")
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
 		text = URL.decodeParam(text);
 		List<Post> list = postService.findByTitle(text);
@@ -36,6 +44,7 @@ public class PostResource {
 	}
 	
 	@GetMapping(value="/fullsearch")
+	@ApiOperation(value="Retorna todos os post que contém o texto, ou a data mínima, ou a data máxima informada")
 	public ResponseEntity<List<Post>> fullSearch(
 			@RequestParam(value="text", defaultValue="") String text,
 			@RequestParam(value="minDate", defaultValue="") String minDate,
--- src/main/java/com/gento/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gento/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,14 +22,20 @@ import com.gento.workshopmongo.domain.User;
 import com.gento.workshopmongo.dto.UserDTO;
 import com.gento.workshopmongo.services.UserService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value="/users")
+@Api(value="API REST Posts")
+@CrossOrigin(origins="*")
 public class UserResource {
 	
 	@Autowired
 	private UserService userService;
 
 	@GetMapping
+	@ApiOperation(value="Retorna todos os usuários")
 	public ResponseEntity <List<UserDTO>> findAll() {
 		List<User> list = userService.findAll();
 		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
@@ -36,12 +43,14 @@ public class UserResource {
 	}
 	
 	@GetMapping(value="/{id}")
+	@ApiOperation(value="Retorna um único usuário")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = userService.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 	
 	@PostMapping
+	@ApiOperation(value="Salva um usuário")
 	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
 		User obj = userService.fromDTO(objDto);
 		obj = userService.insert(obj);
@@ -50,12 +59,14 @@ public class UserResource {
 	}
 	
 	@DeleteMapping(value="/{id}")
+	@ApiOperation(value="Deleta um usuário")
  	public ResponseEntity<Void> delete(@PathVariable String id) {
 		userService.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 	
 	@PutMapping(value="/{id}")
+	@ApiOperation(value="Atualiza um usuário")
 	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
 		User obj = userService.fromDTO(objDto);
 		obj.setId(id);
@@ -64,6 +75,7 @@ public class UserResource {
 	}
 	
 	@GetMapping(value="/{id}/posts")
+	@ApiOperation(value="Retorna todos os post de um determinado usuário")
 	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
 		User obj = userService.findById(id);
 		return ResponseEntity.ok().body(obj.getPosts());
